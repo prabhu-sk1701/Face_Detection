b68708c7cfbd1db9f41c6fa2a07187741dc85b71
prabhu-sk1701 <prabhu.sk1701@gmail.com>
2024-10-12 22:58:56 +0530

    Face detection system

--- face_detection/views.py
+++ face_detection/views.py
@@ -1,3 +1,34 @@
+import cv2
+from django.http import StreamingHttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+def index(request):
+    return render(request, 'face_detection/index.html')
+
+def gen():
+    cap = cv2.VideoCapture(0)
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+        
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, 1.3, 6)
+
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+
+        ret, jpeg = cv2.imencode('.jpg', frame)
+        frame = jpeg.tobytes()
+
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+    cap.release()
+
+def video_feed(request):
+    return StreamingHttpResponse(gen(), content_type='multipart/x-mixed-replace; boundary=frame')
